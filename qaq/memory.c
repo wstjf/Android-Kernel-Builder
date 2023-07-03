196416df2d4aa49cce92a9b799306b8e3ad4d9a5
wstjf <wstjf@qq.com>
2023-07-03 18:24:07 +0800

    test

--- qaq/memory.c
+++ qaq/memory.c
@@ -13,7 +13,7 @@
 #include <asm/pgtable.h>
 
 extern struct mm_struct *get_task_mm(struct task_struct *task);
-extern void mmput(struct mm_struct *);
+extern int mmput(struct mm_struct *);
 #if(LINUX_VERSION_CODE >= KERNEL_VERSION(5, 4, 61))
 
 phys_addr_t translate_linear_address(struct mm_struct* mm, uintptr_t va) {
--- qaq/process.c
+++ qaq/process.c
@@ -10,7 +10,7 @@
 extern struct mm_struct *get_task_mm(struct task_struct *task);
 
 //#if(LINUX_VERSION_CODE >= KERNEL_VERSION(5, 4, 61))
-extern void mmput(struct mm_struct *);
+extern int mmput(struct mm_struct *);
 //#endif
 
 uintptr_t get_module_base(pid_t pid, char* name) 
